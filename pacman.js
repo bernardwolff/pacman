fd90fba7e7b4bb86b943ecfe391452cc194f363d
Bernard Wolff <dranreb@fflow.org>
2016-12-30 20:11:07 -0500

    animate mouth open-close

--- pacman.js
+++ pacman.js
@@ -1,6 +1,6 @@
 function init() {
-  var screen = {width: 224, height: 288, bgColor: "black"},
-      pacman = {x: 112, y: 216, radius: 10, color: "yellow"};
+  var screen = {width: 224, height: 288, bgColor: "black", mouthOpen: true},
+      pacman = {x: 112, y: 216, radius: 10, color: "yellow", mouthOpen: true};
 
   var svg = d3.select("body").append("svg")
     .attr("width", screen.width)
@@ -9,15 +9,34 @@ function init() {
 
   var arc = d3.arc()
     .innerRadius(0)
-    .outerRadius(function(p){console.log(p); return p.radius; })
-    .startAngle(-Math.PI / 4)
-    .endAngle(5/4 * Math.PI)(pacman);
+    .outerRadius(function (p) { return p.radius; })
+    .startAngle(function (p) {
+      if (p.mouthOpen) return -Math.PI / 4;
+      else return -Math.PI / 2;
+    })
+    .endAngle(function (p) {
+      if (p.mouthOpen) return 5/4 * Math.PI;
+      else return 3 * Math.PI / 2;
+    });
 
-  svg.append("g")
+  var p_element = svg.append("g")
     .attr("transform", "translate(" + pacman.x + "," + pacman.y + ")")
     .append("path")
-    .attr("d", arc)
+    .attr("d", arc(pacman))
     .style("fill", pacman.color);
+
+  d3.interval(function() {
+    pacman.mouthOpen = !pacman.mouthOpen;
+    p_element.transition()
+        .duration(1)
+        .attrTween("d", function (d) {
+          return function(t) {
+            return arc(pacman);
+          }
+        });
+  }, 100);
+
+
 }
 
 ready(init);
